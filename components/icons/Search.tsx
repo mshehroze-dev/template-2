56cea5cd1529c4c51921467d559160573eeabed2
mshehroze-dev <m.shehroze@nuvra.agency>
2026-01-22 17:45:35 +0500

    Add components/icons/Search.tsx

---
+++ components/icons/Search.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from 'react'
+
+function Search(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        d="M15 14.4722C16.2275 13.3736 17 11.777 17 10C17 6.68629 14.3137 4 11 4C7.68629 4 5 6.68629 5 10C5 13.3137 7.68629 16 11 16C12.5367 16 13.9385 15.4223 15 14.4722ZM15 14.4722L19.5278 19"
+        stroke="#502CEF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Search
+
