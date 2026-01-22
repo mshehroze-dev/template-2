e5db06274e2ddbd6c4cfcf66a51009a71104819f
mshehroze-dev <m.shehroze@nuvra.agency>
2026-01-22 17:45:23 +0500

    Add components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Menu } from './icons'
+import LogoMark from './LogoMark'
+import MobileMenu from './MobileMenu'
+import Navigation from './Navigation'
+
+function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  return (
+    <></>
+  )
+}
+
+export default Header
